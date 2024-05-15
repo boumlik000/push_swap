18aa44048af96bd70ffe1759960ee38b65bc52f2
mboumlik <mboumlik@gmail.com>
2024-05-15 10:20:18 +0100

    push 1

---
+++ mandatory/get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mboumlik <mboumlik@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/22 12:59:21 by mboumlik          #+#    #+#             */
+/*   Updated: 2024/04/25 10:08:47 by mboumlik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strjoin1(char *str);
+int		ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+#endif
---
+++ mandatory/push_swap.h
@@ -0,0 +1,13 @@
+#ifndef PUSH_SWAP
+#define PUSH_SWAP
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "./get_next_line/get_next_line.h"
+#include "./ft_printf/ft_printf.h"
+
+
+
+
+#endif
